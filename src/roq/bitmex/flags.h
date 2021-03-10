f580ed1fb757ff118daff3a713a176cded3ba656
thraneh <thraneh@gmail.com>
2021-03-10 11:52:24 +0100

    ensure all usage of flags (using abseil-cpp) will compile with the default standard version

---
+++ src/roq/bitmex/flags.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/bitmex/flags/flags.h"
+
+namespace roq {
+namespace bitmex {
+
+using flags::Flags;
+
+}  // namespace bitmex
+}  // namespace roq
