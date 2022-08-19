9528a5ba9a1014a676ad23cefb041216cb733f5a
thraneh <thraneh@gmail.com>
2022-08-19 19:56:23 +0200

    update submodules

--- src/roq/bitmex/prod.sh
+++ src/roq/bitmex/prod.sh
@@ -1,28 +1,43 @@
 #!/usr/bin/env bash
 
-CWD="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+NAME="bitmex"
 
-if [ "$1" == "debug" ]; then
-	PREFIX="gdb --args"
-else
-	PREFIX=
-fi
-
-NAME="bitmex-prod"
-
-CONFIG_FILE="$CWD/config/$NAME.toml"
+CONFIG_FILE="config/$NAME-prod.toml"
 
 URI="bitmex.com"
 
 REST_URI="https://www.$URI"
 WS_URI="wss://ws.$URI/realtime"
 
+# debug?                                                                                                                   
+                                                                                                                           
+if [ "$1" == "debug" ]; then                                                                                               
+  KERNEL="$(uname -a)"                                                                                                     
+  case "$KERNEL" in                                                                                                        
+    Linux*)                                                                                                                
+      PREFIX="gdb --args"                                                                                                  
+      ;;                                                                                                                   
+    Darwin*)                                                                                                               
+      PREFIX="lldb --"                                                                                                     
+      ;;                                                                                                                   
+  esac                                                                                                                                                                       
+  shift 1                                                                                                               
+else                                                                                                                    
+  PREFIX=                                                                                                               
+fi                                                                                                                      
+                                                                                                                        
+# launch
+
+echo "$WS_URI"
+echo "$REST_URI"
+
+
 $PREFIX ./roq-bitmex \
-	--name "bitmex" \
+	--name "$NAME" \
 	--config_file "$CONFIG_FILE" \
-  --event_log_dir "$HOME/var/lib/roq/data" \                                                                            
-  --event_log_symlink \                                                                                                 
-  --client_listen_address "$HOME/run/$NAME.sock" \                                                                      
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --event_log_symlink \
+  --client_listen_address "$HOME/run/$NAME.sock" \
   --metrics_listen_address "$HOME/run/${NAME}_metrics.sock" \
 	--ws_uri "$WS_URI" \
 	--rest_uri "$REST_URI" \
