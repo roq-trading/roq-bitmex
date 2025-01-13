c3fcba8a746efe69f4559331db83f5a48b442277
thraneh <thraneh@gmail.com>
2025-01-13 04:27:20 +0100

    server/gateway mass-quote interfaces

--- src/roq/bitmex/gateway.cpp
+++ src/roq/bitmex/gateway.cpp
@@ -4,6 +4,8 @@
 
 #include "roq/logging.hpp"
 
+#include "roq/server/oms/exceptions.hpp"
+
 using namespace std::literals;
 
 namespace roq {
@@ -110,6 +112,14 @@ uint16_t Gateway::operator()(Event<CancelAllOrders> const &event, std::string_vi
   if (shared_.settings.misc.oms_using_web_socket)
     return get_web_socket(event.value.account)(event, request_id);
   return get_order_entry(event.value.account)(event, request_id);
+}
+
+uint16_t Gateway::operator()(Event<MassQuote> const &) {
+  throw server::oms::NotSupported{"not supported"sv};
+}
+
+uint16_t Gateway::operator()(Event<CancelQuotes> const &) {
+  throw server::oms::NotSupported{"not supported"sv};
 }
 
 void Gateway::operator()(metrics::Writer &writer) {
--- src/roq/bitmex/gateway.hpp
+++ src/roq/bitmex/gateway.hpp
@@ -58,6 +58,10 @@ struct Gateway final : public server::Handler, public OrderEntry::Handler, publi
       Event<CancelOrder> const &, server::oms::Order const &, std::string_view const &request_id, std::string_view const &previous_request_id) override;
   uint16_t operator()(Event<CancelAllOrders> const &, std::string_view const &request_id) override;
 
+  uint16_t operator()(Event<MassQuote> const &) override;
+
+  uint16_t operator()(Event<CancelQuotes> const &) override;
+
   void operator()(metrics::Writer &) override;
 
  private:
