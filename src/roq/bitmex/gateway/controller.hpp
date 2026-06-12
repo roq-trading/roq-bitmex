856d0bcb470248d666177c76ebe50802ac85d68c
thraneh <thraneh@gmail.com>
2026-06-12 08:58:26 +0000

    move parsing of api into gateway library

--- src/roq/bitmex/gateway/controller.hpp
+++ src/roq/bitmex/gateway/controller.hpp
@@ -31,6 +31,8 @@ struct MarketData;
 struct Controller final : public server::Handler, public OrderEntry::Handler, public DropCopy::Handler, public MarketData::Handler {
   ROQ_PUBLIC static std::unique_ptr<server::Handler> create(server::Dispatcher &, Settings const &, Config const &, io::Context &);
 
+  ROQ_PUBLIC static uint8_t parse_api(Settings const &);
+
   Controller(server::Dispatcher &, Settings const &, Config const &, io::Context &);
 
   Controller(Controller const &) = delete;
