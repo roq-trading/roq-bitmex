87acd13f522f90c9e9481fd3cb4448b3e7f7ef52
thraneh <thraneh@gmail.com>
2026-09-15 13:43:26 +0000

    Adding ReferenceData.market_segment and MarketSegmentStatus #636

--- src/roq/bitmex/gateway/product.cpp
+++ src/roq/bitmex/gateway/product.cpp
@@ -192,6 +192,7 @@ ReferenceData Product::reference_data(protocol::json::InstrumentDataItem const &
       .description = {},
       .security_type = {},  // XXX typ?
       .external_security_id = {},
+      .market_segment = {},
       .cfi_code = {},
       .base_currency = settl_currency_,
       .quote_currency = quote_currency_,
